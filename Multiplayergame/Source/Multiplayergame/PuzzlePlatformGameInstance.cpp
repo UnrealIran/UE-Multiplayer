de4664ddb08f17c0de48c04f4b1c8205c854acda
Reza Darooei <reza.darooei@gmail.com>
2019-03-10 12:48:58 +0330

    016 When To Use A GameInstance

---
+++ Multiplayergame/Source/Multiplayergame/PuzzlePlatformGameInstance.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PuzzlePlatformGameInstance.h"
+
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer & ObjectInitializer)
+{
+	UE_LOG(LogTemp,Warning,TEXT("You are in constructor"))
+}
+
+void UPuzzlePlatformGameInstance::Init()
+{
+	Super::Init();
+	UE_LOG(LogTemp,Warning,TEXT("You are in init"))
+}
